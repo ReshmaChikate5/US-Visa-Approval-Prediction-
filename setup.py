176a22dcff9f76ca64bb13335d7ed24423f26e12
Reshma <reshmachikate5@gmail.com>
2024-02-21 22:13:36 +0530

    reuirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Reshma",
+    author_email="reshmachikate5@gmail.com",
+    packages=find_packages()
+)
